63b47cc6d07c415df257dd5723b156587f7d5a77
luisc <luisc@elea.(none)>
2014-01-16 11:12:44 +0100

    some work

--- main.py
+++ main.py
@@ -2,5 +2,11 @@
 
 from parsers.asosparser import AsosParser
 
+def main():
+    url = "http://www.asos.com/es/Su%C3%A9ter-estilo-marinero-de-Esprit/6sh7u/?iid=3454356&cid=6993&sh=0&pge=0&pgesize=36&sort=-1&clr=Natural+white&mporgp=L0VzcHJpdC9Fc3ByaXQtRmlzaGVybWFucy1KdW1wZXIvUHJvZC8."
+    ap = AsosParser()
+    ap.parse(url)
+    print(ap.price)
+
 if __name__ == "__main__":
-    pass
+    main()
--- parsers/webparser.py
+++ parsers/webparser.py
@@ -5,7 +5,6 @@ import requests
 from bs4 import BeautifulSoup
 from model.price import Price
 
-URLs = ["http://www.asos.com/es/ASOS/ASOS-Denim-Shirt-in-Long-Sleeve-with-Leopard-Print/Prod/pgeproduct.aspx?iid=3520721&cid=3602&sh=0&pge=0&pgesize=36&sort=-1&clr=Grey", "http://www.asos.com/es/Vaqueros-acampanados-con-lavado-medio-de-ASOS/545mu/?iid=2962382&cid=5230&sh=0&pge=0&pgesize=36&sort=-1&clr=Blue&mporgp=L0FTT1MvQVNPUy1GbGFyZS1KZWFuLUluLU1pZC1XYXNoL1Byb2Qv"]
 
 class WebParser:
    
@@ -15,6 +14,7 @@ class WebParser:
     def parse(url):
         r = requests.get(url)
         soup = BeautifulSoup(r.text)
+        properparse(soup)
 
     def properparse(soup):
         pass
